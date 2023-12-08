25a18ce6dfc4310fb4781cf17b5692117e7a57ac
Daniel Egele <danielegele9@gmail.com>
2023-12-08 17:07:02 +0100

    all added files

---
+++ js/ATM.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    const form = document.getElementById("cash_form");
+    form.addEventListener("send",(event)=>{
+        event.preventDefault();
+        Withdrawal();
+    });
+
+});
+
+function Withdrawal(){
+    let balance = parseInt(document.getElementById("balance").value);
+    let withdraw = parseInt(document.getElementById("withdraw").value);
+    let cash_form = document.getElementById("cash_form");
+    let para = document.getElementById("para1");
+    let para2 = document.getElementById("para2");
+    let color = document.getElementById("color");
+    let bala = document.getElementById("bala");
+    let button = document.getElementById("send");
+    let ba = document.getElementById("ba");
+
+    if(withdraw<=balance){
+        let remainingBalance = balance-withdraw;
+        alert("You have successfully withdrawn "+ withdraw + " from your account.");
+        document.getElementById("balance").value = remainingBalance;
+        balance.classList.add("withdrawal_form");
+        cash_form.classList.add("cash_form");
+        para.classList.add("para1")
+        para2.classList.add("para2");
+        color.classList.add("color");
+        bala.classList.add("bala");
+        button.classList.add("send");
+        ba.classList.add()
+
+
+    }else{
+        document.getElementById("error").innerHTML;
+        alert("insufficeint funds");
+
+
+    }
+
+    // if(withdraw ===""){
+    //     balance = balance;
+    //     alert("enter an amount as your balance is:"+ balance);
+    // }else{
+    //     document.getElementById("error").innerHTML;
+    //     alert("insufficient funds");
+
+    // }
+}
